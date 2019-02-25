77646702a8c439c5ce2f5cd6ce9f5371b116b15e
yvln <sayyveline@gmail.com>
2019-02-25 08:09:41 +0100

    Add Prettier

--- src/Components/Alert.test.js
+++ src/Components/Alert.test.js
@@ -1,25 +1,25 @@
-import { configure, mount, shallow } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-import React from "react";
-import renderer from "react-test-renderer";
+import { configure, mount, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import React from 'react';
+import renderer from 'react-test-renderer';
 
-import { initializeFirebase } from "../firebase";
+import { initializeFirebase } from '../firebase';
 
-import Alert from "./Alert";
+import Alert from './Alert';
 
 configure({ adapter: new Adapter() });
 
-describe("src/Components/Alert", () => {
-  it("should render", () => {
+describe('src/Components/Alert', () => {
+  it('should render', () => {
     initializeFirebase();
     const component = renderer.create(
       <Alert
         data={{
-          asset: "BTC",
-          amount: "600",
-          action: "above",
-          currency: "USD",
-          email: "itsme@contact.com"
+          asset: 'BTC',
+          amount: '600',
+          action: 'above',
+          currency: 'USD',
+          email: 'itsme@contact.com',
         }}
       />
     );
@@ -29,21 +29,21 @@ describe("src/Components/Alert", () => {
 
   it("should display 'Loading' when no data props", () => {
     const alert = shallow(<Alert />);
-    expect(alert.text()).toEqual("Loading...");
+    expect(alert.text()).toEqual('Loading...');
   });
 
-  it("should display an alert when receiving data props", () => {
+  it('should display an alert when receiving data props', () => {
     const alert = mount(
       <Alert
         data={{
-          asset: "BTC",
-          amount: "600",
-          action: "above",
-          currency: "USD",
-          email: "itsme@contact.com"
+          asset: 'BTC',
+          amount: '600',
+          action: 'above',
+          currency: 'USD',
+          email: 'itsme@contact.com',
         }}
       />
     );
-    expect(alert.text()).toContain("When BTC is above 600USD");
+    expect(alert.text()).toContain('When BTC is above 600USD');
   });
 });
--- src/Components/Form.test.js
+++ src/Components/Form.test.js
@@ -1,27 +1,27 @@
-import { configure, mount } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-import React from "react";
-import renderer from "react-test-renderer";
+import { configure, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import React from 'react';
+import renderer from 'react-test-renderer';
 
-import { initializeFirebase } from "../firebase";
+import { initializeFirebase } from '../firebase';
 
-import Form from "./Form";
+import Form from './Form';
 
 configure({ adapter: new Adapter() });
 
-describe("src/Components/Form", () => {
-  it("should render", () => {
+describe('src/Components/Form', () => {
+  it('should render', () => {
     initializeFirebase();
     const component = renderer.create(
       <Form
         initialValues={{
           data: {
-            asset: "BTC",
-            amount: "600",
-            action: "above",
-            currency: "USD",
-            email: "itsme@contact.com"
-          }
+            asset: 'BTC',
+            amount: '600',
+            action: 'above',
+            currency: 'USD',
+            email: 'itsme@contact.com',
+          },
         }}
       />
     );
@@ -29,27 +29,27 @@ describe("src/Components/Form", () => {
     expect(component).toMatchSnapshot();
   });
 
-  it("should render the Edition From when there are initial values", () => {
+  it('should render the Edition From when there are initial values', () => {
     const form = mount(
       <Form
         initialValues={{
           data: {
-            asset: "BTC",
-            amount: "600",
-            action: "above",
-            currency: "USD",
-            email: "itsme@contact.com"
-          }
+            asset: 'BTC',
+            amount: '600',
+            action: 'above',
+            currency: 'USD',
+            email: 'itsme@contact.com',
+          },
         }}
       />
     );
-    expect(form.find(".FormEdit")).toHaveLength(1);
-    expect(form.find(".FormCreate")).toHaveLength(0);
+    expect(form.find('.FormEdit')).toHaveLength(1);
+    expect(form.find('.FormCreate')).toHaveLength(0);
   });
 
-  it("should render the Creation From when there are no initial values", () => {
+  it('should render the Creation From when there are no initial values', () => {
     const form = mount(<Form />);
-    expect(form.find(".FormCreate")).toHaveLength(1);
-    expect(form.find(".FormEdit")).toHaveLength(0);
+    expect(form.find('.FormCreate')).toHaveLength(1);
+    expect(form.find('.FormEdit')).toHaveLength(0);
   });
 });
